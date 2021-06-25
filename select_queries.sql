f84baa243ea2cc109110df2f16b6343b80a49154
Lorena Campuzano <lorenacampuzano.s@gmail.com>
2021-06-25 14:19:29 -0500

    Queries usando SELECT

---
+++ select_queries.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM posts;
+
+SELECT title, publication_date, estatus
+FROM posts;
+
+SELECT title AS encabezado, publication_date AS publicado, estatus AS estado
+FROM posts;
+
+SELECT COUNT(*) numero_posts
+FROM posts;
