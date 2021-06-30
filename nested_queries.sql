1ae7cdf7b58b843a3615f908f3c35e4df0b7efd5
Lorena Campuzano <lorenacampuzano.s@gmail.com>
2021-06-30 03:46:32 -0500

    Nested queries usando INNER JOIN

--- nested_queries.sql
+++ nested_queries.sql
@@ -15,3 +15,25 @@ WHERE publication_date = (
 	SELECT MAX(publication_date)
     FROM posts
 );
+
+SELECT label.name_label AS post_label, COUNT(*) label_quantity
+FROM(
+	SELECT idPosts, idLabels, name_label
+    FROM posts_labels
+    INNER JOIN labels
+    ON posts_labels.idLabels = labels.id_labels
+) AS label
+GROUP BY label.name_label
+ORDER BY label_quantity DESC;
+
+SELECT title AS post_title, label.name_label AS post_label
+FROM(
+	SELECT idPosts, idLabels, name_label
+    FROM posts_labels
+    INNER JOIN labels
+    ON posts_labels.idLabels = labels.id_labels
+) AS label
+INNER JOIN posts
+ON label.idPosts = posts.id_posts
+GROUP BY post_label;
+
